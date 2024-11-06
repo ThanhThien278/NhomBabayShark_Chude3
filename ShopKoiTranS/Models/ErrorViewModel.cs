2e06edacb841eedf41f6c17f1257c4a9c705dc3e
Shin15 <142900712+hoango15@users.noreply.github.com>
2024-11-06 14:39:22 +0700

    Delete ShopKoiTranS directory

--- ShopKoiTranS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShopKoiTranS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
