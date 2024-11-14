38af9f6d27b1bbadb402d787965903865691d559
phu190605 <trongphu1906@gmail.com>
2024-11-14 15:22:44 +0700

    advise

--- ShopKoiTranS/Controllers/AdviseController.cs
+++ ShopKoiTranS/Controllers/AdviseController.cs
@@ -1,12 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopKoiTranS.Models;
+using ShopKoiTranS.Repository;
+using System;
 
 namespace ShopKoiTranS.Controllers
 {
     public class AdviseController : Controller
     {
+        private readonly DataContext _context;
+
+        public AdviseController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitAdvise(string customerName, string customerPhone, string diaDiem, string moTa)
+        {
+            if (ModelState.IsValid)
+            {
+                // Tạo một bản ghi mới cho tư vấn
+                AdviseModel newAdvise = new AdviseModel
+                {
+                    CustomerName = customerName,
+                    CustomerPhone = customerPhone,
+                    DiaDiem = diaDiem,
+                    MoTa = moTa,
+                    TrangThai = "Chờ xác nhận",
+                    ThoiGianTuVan = DateTime.Now
+                };
+
+                // Thêm vào cơ sở dữ liệu
+                _context.LichTuVans.Add(newAdvise);
+                _context.SaveChanges();
+
+
+                ViewBag.SuccessMessage = "Đặt lịch tư vấn thành công!";
+                ViewBag.AdviseDetails = newAdvise;
+
+                return View("Index");
+            }
+
+            // Thông báo lỗi nếu có
+            ViewBag.ErrorMessage = "Có lỗi xảy ra, vui lòng kiểm tra lại các thông tin.";
+            return View("Index");
         }
     }
 }
--- ShopKoiTranS/Models/AdviseModel.cs
+++ ShopKoiTranS/Models/AdviseModel.cs
@@ -1,6 +1,34 @@
-﻿namespace ShopKoiTranS.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopKoiTranS.Models
 {
     public class AdviseModel
     {
+        [Key]
+        public int AdviseId { get; set; }
+
+        [Required]
+        [Display(Name = "Họ và Tên")]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Số Điện Thoại")]
+        public string CustomerPhone { get; set; }
+
+        [Display(Name = "Thời gian tư vấn")]
+        public DateTime? ThoiGianTuVan { get; set; }
+
+        [Required]
+        [Display(Name = "Địa Điểm")]
+        public string DiaDiem { get; set; }
+
+        [Required]
+        [Display(Name = "Mô Tả")]
+        public string MoTa { get; set; }
+
+        [Required]
+        public string TrangThai { get; set; }
     }
 }
